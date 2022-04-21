2f0d1fede1158bc0b6b859dc819486cd1e438349
tobealive <tobmoj@web.de>
2022-04-21 16:43:28 +0200

    Overhaul test.ts
    - Reduce redudancy
    - More expressive variable naming
    - Get closer to examples in anchor books

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -6,113 +6,115 @@ import * as bs58 from "bs58";
 
 describe("solana-twitter", () => {
 	const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
-	const provider = anchor.AnchorProvider.local();
+	const programProvider = anchor.AnchorProvider.env()
 
-	const sendTweet = async (user, tag, content) => {
-		const tweetKeys = anchor.web3.Keypair.generate();
-		await program.rpc.sendTweet(tag, content, {
-			accounts: {
-				tweet: tweetKeys.publicKey,
-				user,
-				systemProgram: anchor.web3.SystemProgram.programId,
-			},
-			signers: [tweetKeys],
-		});
+	// Configure the client to use the local cluster.
+	anchor.setProvider(programProvider);
 
-		return tweetKeys;
-	};
+	// Generate Users
+	const userOne = programProvider.wallet;
+	const otherUser = anchor.web3.Keypair.generate();
+	const confusedUser = anchor.web3.Keypair.generate();
 
 	// Tweet 1
 	it("can send a new tweet", async () => {
-		const tweetKeys = anchor.web3.Keypair.generate();
+		const tweetKeypair = anchor.web3.Keypair.generate();
 		await program.rpc.sendTweet("veganism", "Hummus, am i right 🧆?", {
 			accounts: {
-				tweet: tweetKeys.publicKey,
-				user: provider.wallet.publicKey,
+				tweet: tweetKeypair.publicKey,
+				user: userOne.publicKey,
 				systemProgram: anchor.web3.SystemProgram.programId,
 			},
-			signers: [tweetKeys],
+			// Anchor automatically signs with the conneted wallets keypair 
+			// in scope of this test it's the program's provider.wallet
+			signers: [tweetKeypair],
 		});
 
 		// Fetch the created tweet
-		const tweet = await program.account.tweet.fetch(tweetKeys.publicKey);
+		const tweetState = await program.account.tweet.fetch(tweetKeypair.publicKey);
 
 		// Ensure it has the right data
-		assert.equal(tweet.user.toBase58(), provider.wallet.publicKey.toBase58());
-		assert.equal(tweet.tag, "veganism");
-		assert.equal(tweet.content, "Hummus, am i right 🧆?");
-		assert.ok(tweet.timestamp);
+		assert.equal(tweetState.user.toBase58(), programProvider.wallet.publicKey.toBase58());
+		assert.equal(tweetState.tag, "veganism");
+		assert.equal(tweetState.content, "Hummus, am i right 🧆?");
+		assert.ok(tweetState.timestamp);
 		// console.log('Sent tweet :', tweetAccount);
 	});
 
 	// Tweet 2
 	it("can send a new tweet without a tag", async () => {
-		const tweetKeys = anchor.web3.Keypair.generate();
+		const tweetKeypair = anchor.web3.Keypair.generate();
 		await program.rpc.sendTweet("", "gm", {
 			accounts: {
-				tweet: tweetKeys.publicKey,
-				user: provider.wallet.publicKey,
+				tweet: tweetKeypair.publicKey,
+				user: programProvider.wallet.publicKey,
 				systemProgram: anchor.web3.SystemProgram.programId,
 			},
-			signers: [tweetKeys],
+			signers: [tweetKeypair],
 		});
 
 		// Fetch the created tweet
-		const tweet = await program.account.tweet.fetch(tweetKeys.publicKey);
+		const tweetState = await program.account.tweet.fetch(tweetKeypair.publicKey);
 
 		// Ensure it has the right data
-		assert.equal(tweet.user.toBase58(), provider.wallet.publicKey.toBase58());
-		assert.equal(tweet.tag, "");
-		assert.equal(tweet.content, "gm");
-		assert.ok(tweet.timestamp);
+		assert.equal(tweetState.user.toBase58(), programProvider.wallet.publicKey.toBase58());
+		assert.equal(tweetState.tag, "");
+		assert.equal(tweetState.content, "gm");
+		assert.ok(tweetState.timestamp);
 		// console.log('Tweet sent without a tag: ', tweet);
 	});
-
-	// Generate another user
-	let otherUser = anchor.web3.Keypair.generate();
 
 	// Tweet 3
 	it("can send a new tweet from a different user", async () => {
 		// Airdrop some SOL
-		const signature = await provider.connection.requestAirdrop(
+		const signature = await programProvider.connection.requestAirdrop(
 			otherUser.publicKey,
 			1000000000
 		);
-		await provider.connection.confirmTransaction(signature);
+		await programProvider.connection.confirmTransaction(signature);
 
-		const tweetKeys = anchor.web3.Keypair.generate();
+		const tweetKeypair = anchor.web3.Keypair.generate();
 		await program.rpc.sendTweet("veganism", "Yay Tofu 🍜!", {
 			accounts: {
-				tweet: tweetKeys.publicKey,
+				tweet: tweetKeypair.publicKey,
 				user: otherUser.publicKey,
 				systemProgram: anchor.web3.SystemProgram.programId,
 			},
-			signers: [otherUser, tweetKeys],
+			signers: [otherUser, tweetKeypair],
 		});
 
 		// Fetch the created tweet
-		const tweet = await program.account.tweet.fetch(tweetKeys.publicKey);
+		const tweetState = await program.account.tweet.fetch(tweetKeypair.publicKey);
 
 		// Ensure it has the right data
-		assert.equal(tweet.user.toBase58(), otherUser.publicKey.toBase58());
-		assert.equal(tweet.tag, "veganism");
-		assert.equal(tweet.content, "Yay Tofu 🍜!");
-		assert.ok(tweet.timestamp);
+		assert.equal(tweetState.user.toBase58(), otherUser.publicKey.toBase58());
+		assert.equal(tweetState.tag, "veganism");
+		assert.equal(tweetState.content, "Yay Tofu 🍜!");
+		assert.ok(tweetState.timestamp);
 		// console.log('Tweet by different user: ', tweet);
 	});
 
+	// Helper functions to call the "SendTweet" instruction to stop repeating ourselfs
+	const sendTweet = async (tweetKeypair, user, tag, content) => {
+		// Anchor nests the provider.wallet keypair inside the payer object 
+		// use 'user.payer' as 'user' if tweet is sent from the provider.wallet
+		user.payer != null ? user = user.payer : user
+
+		await program.rpc.sendTweet(tag, content, {
+			accounts: {
+				tweet: tweetKeypair.publicKey,
+				user: user.publicKey,
+				systemProgram: anchor.web3.SystemProgram.programId,
+			},
+			signers: [tweetKeypair, user],
+		});
+	};
+
 	it("cannot provide a tag with more than 50 characters", async () => {
 		try {
-			const tweetKeys = anchor.web3.Keypair.generate();
+			const tweetKeypair = anchor.web3.Keypair.generate();
 			const tagWith51Chars = "x".repeat(51);
-			await program.rpc.sendTweet(tagWith51Chars, "Hummus, am I right?", {
-				accounts: {
-					tweet: tweetKeys.publicKey,
-					user: provider.wallet.publicKey,
-					systemProgram: anchor.web3.SystemProgram.programId,
-				},
-				signers: [tweetKeys],
-			});
+			await sendTweet(tweetKeypair, userOne, tagWith51Chars, "takes over!");
 		} catch (err) {
 			assert.equal(
 				err.error.errorMessage,
@@ -126,16 +128,9 @@ describe("solana-twitter", () => {
 
 	it("cannot provide a content with more than 280 characters", async () => {
 		try {
-			const tweetKeys = anchor.web3.Keypair.generate();
+			const tweetKeypair = anchor.web3.Keypair.generate();
 			const contentWith281Chars = "x".repeat(281);
-			await program.rpc.sendTweet("veganism", contentWith281Chars, {
-				accounts: {
-					tweet: tweetKeys.publicKey,
-					user: provider.wallet.publicKey,
-					systemProgram: anchor.web3.SystemProgram.programId,
-				},
-				signers: [tweetKeys],
-			});
+			await sendTweet(tweetKeypair, userOne, "veganism", contentWith281Chars);
 		} catch (err) {
 			assert.equal(
 				err.error.errorMessage,
@@ -155,7 +150,7 @@ describe("solana-twitter", () => {
 	});
 
 	it("can filter tweets by user", async () => {
-		const userPublicKey = provider.wallet.publicKey;
+		const userPublicKey = programProvider.wallet.publicKey;
 		const tweets = await program.account.tweet.all([
 			{
 				memcmp: {
@@ -202,52 +197,50 @@ describe("solana-twitter", () => {
 
 	// Tweet 4
 	it("can update a tweet", async () => {
-		const user = provider.wallet.publicKey;
+		const tweetKeypair = anchor.web3.Keypair.generate();
 		// Use the sendTweet function to call the "SendTweet" instruction
-		// ... send another tweet from the programs provider.wallet which anchor automatically signs with its keypair
-		const tweet = await sendTweet(user, "web3", "takes over");
-
+		await sendTweet(tweetKeypair, userOne, "web3", "takes over!");
 		// Fetch its details
-		const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+		const tweet = await program.account.tweet.fetch(tweetKeypair.publicKey);
 
 		// Check if it was received correctly
-		assert.equal(tweetAccount.tag, "web3");
-		assert.equal(tweetAccount.content, "takes over");
-		assert.equal(tweetAccount.edited, false);
+		assert.equal(tweet.tag, "web3");
+		assert.equal(tweet.content, "takes over!");
+		assert.equal(tweet.edited, false);
 
 		// Update the tweet
 		await program.rpc.updateTweet("baneyneys", "And lobsters!", {
 			accounts: {
-				tweet: tweet.publicKey,
-				user,
+				tweet: tweetKeypair.publicKey,
+				user: userOne.publicKey
 			},
 		});
 
 		// Check if the tweet was updated
-		const updatedTweetAccount = await program.account.tweet.fetch(
-			tweet.publicKey
+		const updatedTweet = await program.account.tweet.fetch(
+			tweetKeypair.publicKey
 		);
-		assert.equal(updatedTweetAccount.tag, "baneyneys");
-		assert.equal(updatedTweetAccount.content, "And lobsters!");
-		assert.equal(updatedTweetAccount.edited, true);
+		assert.equal(updatedTweet.tag, "baneyneys");
+		assert.equal(updatedTweet.content, "And lobsters!");
+		assert.equal(updatedTweet.edited, true);
 	});
 
 	// Tweet 5
 	it("cannot update a tweet that wasn't changed", async () => {
 		try {
-			const user = provider.wallet.publicKey;
-			const tweet = await sendTweet(user, "web3", "takes over!");
-			const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+			const tweetKeypair = anchor.web3.Keypair.generate();
+			await sendTweet(tweetKeypair, userOne, "web3", "takes over!");
+			const tweet = await program.account.tweet.fetch(tweetKeypair.publicKey);
 
 			// Check if it was received correctly
-			assert.equal(tweetAccount.tag, "web3");
-			assert.equal(tweetAccount.content, "takes over!");
-			assert.equal(tweetAccount.edited, false);
+			assert.equal(tweet.tag, "web3");
+			assert.equal(tweet.content, "takes over!");
+			assert.equal(tweet.edited, false);
 
 			await program.rpc.updateTweet("web3", "takes over!", {
 				accounts: {
-					tweet: tweet.publicKey,
-					user,
+					tweet: tweetKeypair.publicKey,
+					user: userOne.publicKey
 				},
 			});
 		} catch (err) {
@@ -262,106 +255,86 @@ describe("solana-twitter", () => {
 
 	// Tweet 6
 	it("can upvote a tweet", async () => {
-		const goodTweet = anchor.web3.Keypair.generate();
-
-		await program.rpc.sendTweet(
-			"opensource",
-			"don't forget about the GNU in linux 🦬",
-			{
-				accounts: {
-					tweet: goodTweet.publicKey,
-					user: otherUser.publicKey,
-					systemProgram: anchor.web3.SystemProgram.programId,
-				},
-				signers: [otherUser, goodTweet],
-			}
-		);
-		const tweetAccount = await program.account.tweet.fetch(goodTweet.publicKey);
+		const goodTweetKeypair = anchor.web3.Keypair.generate();
+		await sendTweet(goodTweetKeypair, userOne, "web3", "takes over");
+		const goodTweet = await program.account.tweet.fetch(goodTweetKeypair.publicKey);
 
 		// Check that the tweet has no rating yet
-		assert.ok(tweetAccount.rating == 0);
+		assert.ok(goodTweet.rating == 0);
 
 		// Upvote the tweet
 		await program.rpc.upvoteTweet({
 			accounts: {
-				tweet: goodTweet.publicKey,
+				tweet: goodTweetKeypair.publicKey,
 			},
 		});
 
 		// Check if the tweet was upvoted
-		const upvotedTweetAccount = await program.account.tweet.fetch(
-			goodTweet.publicKey
+		const upvotedTweet = await program.account.tweet.fetch(
+			goodTweetKeypair.publicKey
 		);
 
-		assert.ok(upvotedTweetAccount.rating == 1);
-		console.log("Rating: ", upvotedTweetAccount.rating);
+		assert.ok(upvotedTweet.rating == 1);
+		console.log("Rating: ", upvotedTweet.rating);
 	});
 
 	// Define on global scope so it can be downvoted multiple times
-	let badTweet;
+	let badTweetKeypair = anchor.web3.Keypair.generate();
 
 	// Tweet 7
 	it("can downvote a tweet", async () => {
 		// Airdrop some SOL
-		const confusedUser = anchor.web3.Keypair.generate();
-		const signature = await provider.connection.requestAirdrop(
+		const signature = await programProvider.connection.requestAirdrop(
 			confusedUser.publicKey,
 			1000000000
 		);
-		await provider.connection.confirmTransaction(signature);
+		await programProvider.connection.confirmTransaction(signature);
 
-		badTweet = anchor.web3.Keypair.generate();
-		await program.rpc.sendTweet("hejustwantsourbest", "i like bill gates", {
-			accounts: {
-				tweet: badTweet.publicKey,
-				user: confusedUser.publicKey,
-				systemProgram: anchor.web3.SystemProgram.programId,
-			},
-			signers: [confusedUser, badTweet],
-		});
-
-		const badTweetAccount = await program.account.tweet.fetch(
-			badTweet.publicKey
+		await sendTweet(badTweetKeypair, confusedUser, "hejustwantsourbest", "i like bill gates");
+		const badTweet = await program.account.tweet.fetch(
+			badTweetKeypair.publicKey
 		);
+
 		// Check that the tweet has no rating yet
-		assert.ok(badTweetAccount.rating == 0);
+		assert.ok(badTweet.rating == 0);
 
 		// Downvote that tweet
 		await program.rpc.downvoteTweet({
 			accounts: {
-				tweet: badTweet.publicKey,
+				tweet: badTweetKeypair.publicKey,
 			},
 		});
 
 		// Check if the tweet was downvoted
-		const downvotedTweetAccount = await program.account.tweet.fetch(
-			badTweet.publicKey
+		const downvotedTweet = await program.account.tweet.fetch(
+			badTweetKeypair.publicKey
 		);
 
-		assert.ok(downvotedTweetAccount.rating == -1);
-		console.log("Rating: ", downvotedTweetAccount.rating);
+		assert.ok(downvotedTweet.rating == -1);
+		console.log("Rating: ", downvotedTweet.rating);
 	});
 
 	it("can downvote a tweet again", async () => {
-		const badTweetAccount = await program.account.tweet.fetch(
-			badTweet.publicKey
+		const badTweet = await program.account.tweet.fetch(
+			badTweetKeypair.publicKey
 		);
 		// Check that the tweet was downvoted already
-		assert.ok(badTweetAccount.rating == -1);
+		assert.ok(badTweet.rating == -1);
 
 		// Downvote the tweet again
 		await program.rpc.downvoteTweet({
 			accounts: {
-				tweet: badTweet.publicKey,
+				tweet: badTweetKeypair.publicKey,
 			},
 		});
 
 		// Check if the tweet was downvoted again
-		const downvotedTweetAccount = await program.account.tweet.fetch(
-			badTweet.publicKey
+		const downvotedTweet = await program.account.tweet.fetch(
+			badTweetKeypair.publicKey
 		);
 
-		assert.ok(downvotedTweetAccount.rating == -2);
-		console.log("Rating: ", downvotedTweetAccount.rating);
+		assert.ok(downvotedTweet.rating == -2);
+		console.log("Rating: ", downvotedTweet.rating);
 	});
 });
+

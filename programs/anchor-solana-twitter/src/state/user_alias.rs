fa55ef2da0a0a086457ca0361daf3733d00d2cda
tobealive <tobmoj@web.de>
2022-08-02 16:20:04 +0200

    Fix indentation

--- programs/anchor-solana-twitter/src/state/user_alias.rs
+++ programs/anchor-solana-twitter/src/state/user_alias.rs
@@ -8,7 +8,7 @@ pub struct UserAlias {
 
 #[derive(Accounts)]
 pub struct CreateUserAlias<'info> {
-    // space: 8 discriminator + (4 length prefix + 50 * 4 ) alias + 1 bump
+	// space: 8 discriminator + (4 length prefix + 50 * 4 ) alias + 1 bump
 	#[account(init, payer = user, space = 8 + (4 + 50 * 4) + 1, seeds = [b"user-alias", user.key().as_ref()], bump)]
 	pub user_alias: Account<'info, UserAlias>,
 	pub system_program: Program<'info, System>,
@@ -25,7 +25,7 @@ pub struct UpdateUserAlias<'info> {
 
 #[derive(Accounts)]
 pub struct DeleteUserAlias<'info> {
-    pub user: Signer<'info>,
+	pub user: Signer<'info>,
 	#[account(mut, close = user, seeds = [b"user-alias", user.key().as_ref()], bump = user_alias.bump)]
 	pub user_alias: Account<'info, UserAlias>,
 }

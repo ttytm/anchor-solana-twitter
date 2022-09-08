4dad4b1d31568958ec2b04d7d37f6fe459d5e817
Turiiya <34311583+tobealive@users.noreply.github.com>
2022-09-08 13:13:25 +0200

    Modularize tests (#7)

---
+++ tests/5-alias.ts
@@ -0,0 +1,36 @@
+import * as anchor from "@project-serum/anchor";
+import * as assert from "assert";
+import { PublicKey } from "@solana/web3.js";
+import { program, user } from "../tests";
+
+describe("user alias", () => {
+	it("can create and update a user alias", async () => {
+		const [userAliasPDA, _] = await PublicKey.findProgramAddress(
+			[anchor.utils.bytes.utf8.encode("user-alias"), user.publicKey.toBuffer()],
+			program.programId
+		);
+
+		await program.methods.createUserAlias("Potatogirl")
+			.accounts({ user: user.publicKey, userAlias: userAliasPDA })
+			.rpc();
+		assert.equal((await program.account.userAlias.fetch(userAliasPDA)).alias, "Potatogirl");
+
+		await program.methods.updateUserAlias("Sasha")
+			.accounts({ user: user.publicKey, userAlias: userAliasPDA })
+			.rpc();
+		assert.equal((await program.account.userAlias.fetch(userAliasPDA)).alias, "Sasha");
+
+	});
+
+	it("can delete a user alias", async () => {
+		const [userAliasPDA, _] = await PublicKey.findProgramAddress(
+			[anchor.utils.bytes.utf8.encode("user-alias"), user.publicKey.toBuffer()],
+			program.programId
+		);
+
+		await program.methods.deleteUserAlias()
+			.accounts({ user: user.publicKey, userAlias: userAliasPDA })
+			.rpc();
+		assert.ok((await program.account.userAlias.fetchNullable(userAliasPDA)) === null);
+	});
+})
